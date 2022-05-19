457f8afa04f17ed09ef50d79cfeb3733fa691136
jtuluve <74163310+jtuluve@users.noreply.github.com>
2022-05-19 18:41:48 +0530

    Update script.js

--- new/script.js
+++ new/script.js
@@ -1,3 +1,23 @@
+document.getElementById("lang").addEventListener("change", lang);
+
+function lang()
+        {
+let lang = document.getElementById("lang").value;
+
+            if (lang == "english"){
+document.getElementById("englang").style.display = "block";
+document.getElementById("tululang").style.display = "none";
+document.getElementById("kanlang").style.display = "none";
+}else if (lang == "tulu"){
+document.getElementById("englang").style.display = "none";
+document.getElementById("tululang").style.display = "block";
+document.getElementById("kanlang").style.display = "none";
+}else if(lang == "kannada"){
+document.getElementById("englang").style.display = "none";
+document.getElementById("tululang").style.display = "none";
+document.getElementById("kanlang").style.display = "block";
+}
+}
 
 function tulu(){
 let tl = document.getElementById("tl");
