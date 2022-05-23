02a529a78227dc81a946169af2e0aead8ead432b
jtuluve <74163310+jtuluve@users.noreply.github.com>
2022-05-23 08:56:29 +0530

    Update script.js

--- k2t/script.js
+++ k2t/script.js
@@ -210,12 +210,8 @@ if (txt[I - 4] === "್" && txt[I - 2] === "್") {
 txt = txt.slice(0, I - 4) + "I" + txt.slice(I - 4, I) + txt.slice(I+1);
 I = txt.indexOf("ೀ"); 
 } else { 
-if (txt[I - 2] === "್") { 
-txt = txt.slice(0, I - 2) + "I" + txt.slice(I - 2, I) + txt.slice(I+1); 
-I = txt.indexOf("ೀ"); } else { 
 txt = txt.slice(0, I) + "I" + txt.slice(I + 1); 
 I = txt.indexOf("ೀ"); 
-}
 }
 }
 
@@ -239,7 +235,7 @@ H = txt.indexOf("ರ್");
 
 
 let rep = txt.replace(/ಅ/g, "AA").replace(/ಆ/g, "Aa").replace(/ಇ/g, "Ai").replace(/ಈ/g, "AI").replace(/ಉ/g, "Au").replace(/ಊ/g, "AU").replace(/ಋ/g, "AR").replace(/ೠ/g, "ARR").replace(/ಎ/g, "eA").replace(/ಏ/g, "EA").replace(/ಐ/g, "eeA").replace(/ಒ/g, "eAa").replace(/ಓ/g, "FAa").replace(/ಔ/g, "AY").replace(/ಂ/g, "M").replace(/ಃ/g, "H").replace(/ಕ/g, "k").replace(/ಖ/g, "K").replace(/ಗ/g, "g").replace(/ಘ/g, "G").replace(/ಙ/g, "Z").replace(/ಚ/g, "c").replace(/ಛ/g, "C").replace(/ಜ/g, "j").replace(/ಝ/g, "J").replace(/ಞ/g, "z").replace(/ಟ/g, "q").replace(/ಠ/g, "Q").replace(/ಡ/g, "w").replace(/ಢ/g, "W").replace(/ಣ/g, "N").replace(/ತ/g, "t").replace(/ಥ/g, "T").replace(/ದ/g, "d").replace(/ಧ/g, "D").replace(/ನ/g, "n").replace(/ಪ/g, "p").replace(/ಫ/g, "P").replace(/ಬ/g, "b").replace(/ಭ/g, "B").replace(/ಮ/g, "m").replace(/ಯ/g, "y").replace(/ರ/g, "r").replace(/ಲ/g, "l").replace(/ವ/g, "v").replace(/ಶ/g, "S").replace(/ಷ/g, "x").replace(/ಸ/g, "s").replace(/ಹ/g, "h").replace(/ಳ/g, "L").replace(/್/g, "A").replace(/ಾ/g, "a").replace(/ು/g, "u").replace(/ೂ/g, "U").replace(/ೌ/g, "Y").replace(/ಿ/g, "i").replace(/ೀ/g, "I").replace(/ೃ/g, "R").replace(/‍/g, "X").replace(/‌/g, "X"); 
-rep = rep.replace(/eee/g, "ee");
+rep = rep.replace(/eXee/g, "eXe");
 document.getElementById("resu").value = rep;
 }
 
