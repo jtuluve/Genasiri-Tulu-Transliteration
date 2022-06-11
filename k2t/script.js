c033c4b6c6bba3ebb532a0eb8e3c9f8f46233a8e
jtuluve <74163310+jtuluve@users.noreply.github.com>
2022-06-11 11:19:49 +0530

    Update script.js

--- k2t/script.js
+++ k2t/script.js
@@ -12,7 +12,7 @@ document.getElementById("tululang2").style.display = "none";
 document.getElementById("kanlang").style.display = "none";
 document.getElementById("kanlang2").style.display = "none";
 
-document.getElementById("uvw").innerHTML = "<textarea type='text' class='txt' id='txt' placeholder='type in kannada'></textarea>";
+document.getElementById("uvw").innerHTML = "<textarea type=" +"'text' " + "class=" +"'txt'" +" id=" + "'txt'"+ " placeholder="+ " 'type in kannada'"+"></textarea>";
 
 }else if (lang == "tulu"){
 
